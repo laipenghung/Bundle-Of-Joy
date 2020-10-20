4b20557aad9e438d22a20a78e3b86602d8842e84
Lai Peng Hung <101208371@students.swinburne.edu.my>
2020-10-20 17:42:52 +0800

    Baby view profile OK

---
+++ main/bundle_of_joy/lib/baby/babyInfo.dart
@@ -0,0 +1,116 @@
+import "package:flutter/cupertino.dart";
+import "package:flutter/material.dart";
+import "package:firebase_auth/firebase_auth.dart";
+import "package:cloud_firestore/cloud_firestore.dart";
+import "baby.dart";
+
+class BabyInfo extends StatefulWidget {
+  final Baby baby;
+  BabyInfo({Key key, this.baby}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _BabyInfo(baby);
+}
+
+class _BabyInfo extends State<BabyInfo>{
+  final Baby baby;
+  _BabyInfo(this.baby);
+  String input;
+
+  ListView _listView(AsyncSnapshot<DocumentSnapshot> document){
+    double fontSizeTitle = MediaQuery.of(context).size.width * 0.05;
+    double fontSizeText = MediaQuery.of(context).size.width * 0.04;
+
+    final _listItems = [
+      "Registered ID",
+      "Name",
+      "Date of Birth",
+      "Time of Birth",
+      "Place of Birth",
+      "Gender",
+      "Age",
+      "Blood Type",
+      "Mode Of Delivery",
+      "Weight At Birth",
+      "Length At Birth",
+      "Head Circumference",
+      "Order"];
+
+    final _listInfo = [
+      baby.b_registered_id,
+      baby.b_name,
+      baby.b_dob.toDate().toString().substring(0,10),
+      baby.b_dob.toDate().toString().substring(11,16),
+      baby.b_place_of_birth,
+      baby.b_gender,
+      baby.b_age,
+      baby.b_bloodType,
+      baby.b_mode_of_delivery,
+      baby.b_weight_at_birth.toString(),
+      baby.b_length_at_birth.toString(),
+      baby.b_head_circumference.toString(),
+      baby.b_order.toString()
+    ];
+
+    return ListView.separated(
+      itemCount: _listItems.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(
+            _listItems[index],
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: "Comfortaa",
+              fontWeight: FontWeight.bold,
+              fontSize: fontSizeText,
+            ),
+          ),
+          trailing: Text(
+            _listInfo[index],
+            style: TextStyle(
+              fontFamily: "Comfortaa",
+              fontSize: fontSizeText,
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (context, index) => Divider(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final User user = FirebaseAuth.instance.currentUser;
+    DocumentReference babyDoc = FirebaseFirestore.instance.collection("mother").doc(user.uid).collection("baby").doc(baby.b_id);
+    double fontSizeTitle = MediaQuery.of(context).size.width * 0.05;
+
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        title: Text(
+          baby.b_name,
+          style: TextStyle(
+            fontFamily: "Comfortaa",
+            fontWeight: FontWeight.bold,
+            fontSize: fontSizeTitle,
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Color(0xFFFCFFD5),
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: StreamBuilder(
+            stream: babyDoc.snapshots(),
+            builder: (context, document){
+              return _listView(document);
+            }
+        ),
+      ),
+    );
+  }
+}
+
--- main/bundle_of_joy/lib/baby/babyProfile.dart
+++ main/bundle_of_joy/lib/baby/babyProfile.dart
@@ -2,6 +2,8 @@ import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import "package:cloud_firestore/cloud_firestore.dart";
+import "baby.dart";
+import "babyInfo.dart";
 
 class BabyProfile extends StatefulWidget {
   @override
@@ -11,12 +13,68 @@ class BabyProfile extends StatefulWidget {
 class _BabyProfile extends State<BabyProfile>{
 
   Widget _listView(AsyncSnapshot<QuerySnapshot> collection){
+    double fontSizeTitle = MediaQuery.of(context).size.width * 0.05;
+    double fontSizeText = MediaQuery.of(context).size.width * 0.04;
+    final _listField = ["b_id", "m_id", "b_registered_id", "b_name", "b_dob", "b_place_of_birth", "b_gender", "b_age", "b_bloodType", "b_mode_of_delivery", "b_weight_at_birth", "b_length_at_birth", "b_head_circumference", "b_order"];
+    List<Baby> _listBaby = List<Baby>();
+    if(collection.data.size != 0){
+      collection.data.docs.forEach((doc) {
+        _listBaby.add(
+          Baby(
+              doc.data()[_listField[0]],
+              doc.data()[_listField[1]],
+              doc.data()[_listField[2]],
+              doc.data()[_listField[3]],
+              doc.data()[_listField[4]],
+              doc.data()[_listField[5]],
+              doc.data()[_listField[6]],
+              doc.data()[_listField[7]],
+              doc.data()[_listField[8]],
+              doc.data()[_listField[9]],
+              doc.data()[_listField[10]].toDouble(),
+              doc.data()[_listField[11]].toDouble(),
+              doc.data()[_listField[12]].toDouble(),
+              doc.data()[_listField[13]].toInt())
+        );
+      });
 
+      return ListView.separated(
+        itemCount: _listBaby.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(_listBaby[index].b_name),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => BabyInfo(baby: _listBaby[index])),
+              );
+            },
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider();
+        },
+      );
+    }
+    else{
+      return Container(
+        child: Center(
+          child: Text(
+            "No Record Found",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: fontSizeTitle,
+              fontFamily: "Comfortaa",
+            ),
+          ),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    double fontSize = MediaQuery.of(context).size.width * 0.05;
     final User user = FirebaseAuth.instance.currentUser;
     Query baby = FirebaseFirestore.instance.collection("mother").doc(user.uid).collection("baby").orderBy("b_name");
     double fontSizeTitle = MediaQuery.of(context).size.width * 0.05;
