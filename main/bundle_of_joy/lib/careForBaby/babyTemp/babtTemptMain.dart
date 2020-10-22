9beef3ca1c05105109f3073c14ed8a9be0a3dc9b
wellson99 <55526214+wellson99@users.noreply.github.com>
2020-10-23 00:55:45 +0800

    Minor Update
    -Added the main page for CareForBaby tabs

---
+++ main/bundle_of_joy/lib/careForBaby/babyTemp/babtTemptMain.dart
@@ -0,0 +1,149 @@
+import "package:flutter/material.dart";
+
+class BabyTempMain extends StatefulWidget {
+  @override
+  _BabyTempMainState createState() => _BabyTempMainState();
+}
+
+class _BabyTempMainState extends State<BabyTempMain> {
+  // BUILD THE WIDGET
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.75,
+                height: MediaQuery.of(context).size.height * 0.2,
+                decoration: myBoxDecoration(),
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                        child: Image.asset(
+                          "assets/icons/menu.png",
+                          height: 70,
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          "Temperature Record",
+                          style: TextStyle(
+                            fontFamily: 'Comfortaa',
+                            fontWeight: FontWeight.bold,
+                            fontSize: MediaQuery.of(context).size.height * 0.025,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              onTap: () {
+                //Navigator.push(
+                  //context,
+                  //MaterialPageRoute(builder: (context) => FoodIntakeListDone()),
+                //);
+              },
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+            InkWell(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.75,
+                height: MediaQuery.of(context).size.height * 0.2,
+                decoration: myBoxDecoration(),
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                        child: Image.asset(
+                          "assets/icons/add.png",
+                          height: 65,
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          "Add Record",
+                          style: TextStyle(
+                            fontFamily: 'Comfortaa',
+                            fontWeight: FontWeight.bold,
+                            fontSize: MediaQuery.of(context).size.height * 0.025,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              onTap: () {
+                //Navigator.push(
+                  //context,
+                  //MaterialPageRoute(builder: (context) => FoodIntakeAdd1()),
+                //);
+              },
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+            InkWell(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.75,
+                height: MediaQuery.of(context).size.height * 0.2,
+                decoration: myBoxDecoration(),
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                        child: Image.asset(
+                          "assets/icons/pending.png",
+                          height: 70,
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          "Pending Record",
+                          style: TextStyle(
+                            fontFamily: 'Comfortaa',
+                            fontWeight: FontWeight.bold,
+                            fontSize: MediaQuery.of(context).size.height * 0.025,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              onTap: () {
+                //Navigator.push(
+                  //context,
+                  //MaterialPageRoute(builder: (context) => FoodIntakeListPending()),
+                //);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration myBoxDecoration() {
+    return BoxDecoration(
+      color: Color(0xFFFCFFD5),
+      border: Border.all(
+        color: Colors.black,
+        width: 2.0,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    );
+  }
+}
