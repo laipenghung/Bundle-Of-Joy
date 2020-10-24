d2f665da3c38e0193db9a51aa304d48d078a5210
noskcid99 <55526183+noskcid99@users.noreply.github.com>
2020-10-24 08:48:41 +0800

    Everything fail

--- main/bundle_of_joy/lib/appointmentMother/appointmentMother_main.dart
+++ main/bundle_of_joy/lib/appointmentMother/appointmentMother_main.dart
@@ -24,8 +24,10 @@ class _AppointmentMotherMainState extends State<AppointmentMotherMain> {
             color: Colors.black,
           ),
         ),
-
-        automaticallyImplyLeading: false, // CENTER THE TEXT
+        iconTheme: IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+        //automaticallyImplyLeading: false, // CENTER THE TEXT
         backgroundColor: Color(0xFFFCFFD5),
         centerTitle: true,
       ),
