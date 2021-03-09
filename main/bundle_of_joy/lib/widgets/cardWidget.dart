15d46a810fc076f968a27867e0089dede6435378
wellson99 <55526214+wellson99@users.noreply.github.com>
2021-03-09 22:21:46 +0800

    UI rework update
    -Added MotherForBaby new UI (Unfinished)
    -MotherToBe UI rework finish

--- main/bundle_of_joy/lib/widgets/cardWidget.dart
+++ main/bundle_of_joy/lib/widgets/cardWidget.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class CategoryCard extends StatelessWidget {
+class CardWidget extends StatelessWidget {
   final String svgSrc;
   final String title;
   final Function press;
-  const CategoryCard({
+  const CardWidget({
     Key key,
     this.svgSrc,
     this.title,
@@ -17,16 +17,16 @@ class CategoryCard extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(13),
       child: Container(
-        padding: EdgeInsets.all(20),
+        padding: EdgeInsets.all(10),
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(13),
           boxShadow: [
             BoxShadow(
               offset: Offset(0, 17),
-              blurRadius: 17,
-              spreadRadius: -23,
-              color:  Color(0xFFE6E6E6),
+              blurRadius: 50,
+              spreadRadius: 23,
+              color: Color(0xFF878787),
             ),
           ],
         ),
