6b48ca8040eb7ad1e2491e165fb7bc7ef7c015db
noskcid99 <55526183+noskcid99@users.noreply.github.com>
2021-03-22 06:27:26 +0800

    New Appointment Add Hospital, Doctor, Date Update

--- main/bundle_of_joy/lib/appointmentMother/appointmentMother_add_full.dart
+++ main/bundle_of_joy/lib/appointmentMother/appointmentMother_add_full.dart
@@ -1,6 +1,7 @@
 import 'package:fa_stepper/fa_stepper.dart';
 import "package:flutter/material.dart";
 import "package:firebase_auth/firebase_auth.dart";
+import "package:cloud_firestore/cloud_firestore.dart";
 import "appointmentMother_add_hospital.dart";
 import "appointmentMother_add_doctor.dart";
 import "appointmentMother_add_date.dart";
@@ -17,97 +18,9 @@ class AppointmentMotherAddFull extends StatefulWidget {
 class _AppointmentMotherAddFullState extends State<AppointmentMotherAddFull> {
   final User user = FirebaseAuth.instance.currentUser;
   final String hospital;
+  int selectedIndex;
 
   _AppointmentMotherAddFullState(this.hospital);
-
-  int _currentStep = 0;
-
-  List<FAStep> _stepper = [
-    // SELECT HOSPITAL /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    FAStep(
-      title: Text(
-        'Select a hospital',
-        style: TextStyle(
-          fontFamily: 'Comfortaa',
-          fontWeight: FontWeight.bold,
-          fontSize: 15,
-          color: Colors.black,
-        ),
-      ),
-      //isActive: true,
-      //state: FAStepstate.disabled,
-      content: Scrollbar(
-        child: Container(
-          height: 350,
-          //color: Colors.blue,
-          child: AppointmentMotherAddHospital(),
-        ),
-      ),
-    ),
-
-    // SELECT DOCTOR /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    FAStep(
-      title: Text(
-        'Select a doctor',
-        style: TextStyle(
-          fontFamily: 'Comfortaa',
-          fontWeight: FontWeight.bold,
-          fontSize: 15,
-          color: Colors.black,
-        ),
-      ),
-      //isActive: true,
-      //state: FAStepstate.disabled,
-      content: Scrollbar(
-        child: Container(
-          height: 350,
-          //color: Colors.blue,
-          child: AppointmentMotherAddDoctor(),
-        ),
-      ),
-    ),
-
-    // SELECT DATE /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    FAStep(
-      title: Text(
-        'Select a date',
-        style: TextStyle(
-          fontFamily: 'Comfortaa',
-          fontWeight: FontWeight.bold,
-          fontSize: 15,
-          color: Colors.black,
-        ),
-      ),
-      //isActive: true,
-      //state: FAStepstate.disabled,
-      content: Container(
-        //color: Colors.blue,
-        child: AppointmentMotherAddDate(),
-      ),
-    ),
-
-    // SELECT TIME /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    FAStep(
-      title: Text(
-        'Select a time',
-        style: TextStyle(
-          fontFamily: 'Comfortaa',
-          fontWeight: FontWeight.bold,
-          fontSize: 15,
-          color: Colors.black,
-        ),
-      ),
-      //isActive: true,
-      //state: FAStepstate.disabled,
-      content: Scrollbar(
-        child: Container(
-          height: 350,
-          //color: Colors.blue,
-          child: AppointmentMotherAddDoctor(),
-        ),
-      ),
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -136,38 +49,171 @@ class _AppointmentMotherAddFullState extends State<AppointmentMotherAddFull> {
 
       // BODY
       body: Container(
-        child: FAStepper(
-            steps: _stepper,
-            stepNumberColor: Colors.grey,
-            //stepNumberColor: Color(0xFFFCFFD5),
-            physics: ClampingScrollPhysics(),
-            currentStep: this._currentStep,
-            onStepTapped: (step) {
-              setState(() {
-                this._currentStep = step;
-              });
-              print('onStepTapped: ' + step.toString());
-            },
-            onStepContinue: () {
-              setState(() {
-                if (this._currentStep < this._stepper.length - 1) {
-                  this._currentStep = this._currentStep + 1;
-                } else {
-                  _currentStep = 0;
-                }
-              });
-              print('onStepContinue: ' + _currentStep.toString());
-            },
-            onStepCancel: () {
-              setState(() {
-                if (this._currentStep > 0) {
-                  this._currentStep = this._currentStep - 1;
-                } else {
-                  this._currentStep = 0;
-                }
-              });
-              print('onStepCancel: ' + _currentStep.toString());
-            }),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // SELECT HOSPITAL /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+              Container(
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02),
+                child: Row(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      child: Expanded(
+                        child: Divider(
+                          indent: MediaQuery.of(context).size.width * 0.0,
+                          endIndent: MediaQuery.of(context).size.width * 0.0,
+                          color: Colors.black,
+                          thickness: MediaQuery.of(context).size.height * 0.0015,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02, right: MediaQuery.of(context).size.width * 0.02),
+                      child: Text(
+                        "Select a hospital",
+                        style: TextStyle(
+                          fontFamily: 'Comfortaa',
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.018,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: MediaQuery.of(context).size.height * 0.0015,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: AppointmentMotherAddHospital(),
+              ),
+
+              // SELECT DOCTOR /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+              Container(
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.04),
+                child: Row(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      child: Expanded(
+                        child: Divider(
+                          indent: MediaQuery.of(context).size.width * 0.0,
+                          endIndent: MediaQuery.of(context).size.width * 0.0,
+                          color: Colors.black,
+                          thickness: MediaQuery.of(context).size.height * 0.0015,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02, right: MediaQuery.of(context).size.width * 0.02),
+                      child: Text(
+                        "Select a doctor",
+                        style: TextStyle(
+                          fontFamily: 'Comfortaa',
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.018,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: MediaQuery.of(context).size.height * 0.0015,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: AppointmentMotherAddDoctor(),
+              ),
+
+              // SELECT DATE /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+              Container(
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.04),
+                child: Row(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      child: Expanded(
+                        child: Divider(
+                          indent: MediaQuery.of(context).size.width * 0.0,
+                          endIndent: MediaQuery.of(context).size.width * 0.0,
+                          color: Colors.black,
+                          thickness: MediaQuery.of(context).size.height * 0.0015,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02, right: MediaQuery.of(context).size.width * 0.02),
+                      child: Text(
+                        "Select a date",
+                        style: TextStyle(
+                          fontFamily: 'Comfortaa',
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.018,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: MediaQuery.of(context).size.height * 0.0015,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              AppointmentMotherAddDate(),
+              // SELECT TIME /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+              Container(
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.04),
+                child: Row(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.1,
+                      child: Expanded(
+                        child: Divider(
+                          indent: MediaQuery.of(context).size.width * 0.0,
+                          endIndent: MediaQuery.of(context).size.width * 0.0,
+                          color: Colors.black,
+                          thickness: MediaQuery.of(context).size.height * 0.0015,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02, right: MediaQuery.of(context).size.width * 0.02),
+                      child: Text(
+                        "Select a time",
+                        style: TextStyle(
+                          fontFamily: 'Comfortaa',
+                          fontWeight: FontWeight.bold,
+                          fontSize: MediaQuery.of(context).size.height * 0.018,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: MediaQuery.of(context).size.height * 0.0015,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- main/bundle_of_joy/lib/appointmentMother/appointmentMother_add_hospital.dart
+++ main/bundle_of_joy/lib/appointmentMother/appointmentMother_add_hospital.dart
@@ -21,6 +21,7 @@ class _AppointmentMotherAddHospitalState extends State<AppointmentMotherAddHospi
   final User user = FirebaseAuth.instance.currentUser;
   _AppointmentMotherAddHospitalState(this.name);
   */
+  int selectedIndex;
 
   _AppointmentMotherAddHospitalState();
 
@@ -45,7 +46,7 @@ class _AppointmentMotherAddHospitalState extends State<AppointmentMotherAddHospi
           } else if (snapshot.data.documents.isEmpty) {
             return Center(
               child: Text(
-                'There is currently no records',
+                'There is currently no hospital available',
                 style: TextStyle(
                   fontFamily: 'Comfortaa',
                   fontSize: MediaQuery.of(context).size.width * 0.04,
@@ -61,126 +62,124 @@ class _AppointmentMotherAddHospitalState extends State<AppointmentMotherAddHospi
                 itemBuilder: (_, index) {
                   return InkWell(
                     onTap: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => AppointmentMotherAddFull(hospital: snapshot.data.documents[index]["h_name"])));
+                      setState(() {
+                        selectedIndex = index;
+                      }); //Rebuild this page
                     },
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.only(
-                            top: MediaQuery.of(context).size.height * 0.015,
-                            bottom: MediaQuery.of(context).size.height * 0.015,
-                          ),
-                          child: Row(
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  // Hospital Name
-                                  Container(
-                                    //color: Colors.lightBlue,
-                                    width: MediaQuery.of(context).size.width * 0.75,
-                                    child: Text(
-                                      snapshot.data.documents[index]['h_name'],
-                                      style: TextStyle(
-                                        fontFamily: 'Comfortaa',
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: MediaQuery.of(context).size.width * 0.04,
-                                        color: Colors.black,
-                                      ),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Container(
+                            color: selectedIndex == index ? Color(0xFFBBBBBB) : Colors.transparent,
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.015, bottom: MediaQuery.of(context).size.height * 0.015),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Hospital Name
+                                Container(
+                                  width: MediaQuery.of(context).size.width * 0.9,
+                                  //color: Colors.lightBlue,
+                                  child: Text(
+                                    snapshot.data.documents[index]['h_name'],
+                                    style: TextStyle(
+                                      fontFamily: 'Comfortaa',
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: MediaQuery.of(context).size.width * 0.04,
+                                      color: Colors.black,
                                     ),
                                   ),
+                                ),
 
-                                  SizedBox(height: MediaQuery.of(context).size.height * 0.025),
+                                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
 
-                                  // Address
-                                  Row(
-                                    children: [
-                                      Container(
-                                        child: Image.asset(
-                                          "assets/icons/address.png",
-                                          height: MediaQuery.of(context).size.height * 0.03, // Icon Size
-                                        ),
-                                      ),
-                                      Container(
-                                        //color: Colors.lightBlue,
-                                        width: MediaQuery.of(context).size.width * 0.67,
-                                        margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02),
-                                        child: Text(
-                                          snapshot.data.documents[index]['h_address'],
-                                          style: TextStyle(
-                                            fontFamily: 'Comfortaa',
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: MediaQuery.of(context).size.height * 0.02,
-                                            color: Colors.black,
+                                // Address
+                                Row(
+                                  children: [
+                                    Container(
+                                      child: Image.asset("assets/icons/address.png", height: MediaQuery.of(context).size.height * 0.03 // Icon Size
                                           ),
+                                    ),
+                                    Container(
+                                      //color: Colors.lightBlue,
+                                      width: MediaQuery.of(context).size.width * 0.8,
+                                      margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02),
+                                      child: Text(
+                                        snapshot.data.documents[index]['h_address'],
+                                        style: TextStyle(
+                                          fontFamily: 'Comfortaa',
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: MediaQuery.of(context).size.height * 0.018,
+                                          color: Colors.black,
                                         ),
                                       ),
-                                    ],
-                                  ),
+                                    ),
+                                  ],
+                                ),
 
-                                  SizedBox(height: MediaQuery.of(context).size.height * 0.025),
+                                SizedBox(height: MediaQuery.of(context).size.height * 0.01),
 
-                                  // Operating Hours
-                                  Row(
-                                    children: [
-                                      Container(
-                                        child: Image.asset(
-                                          "assets/icons/clock.png",
-                                          height: MediaQuery.of(context).size.height * 0.03, // Icon Size
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02),
-                                        child: Text(
-                                          snapshot.data.documents[index]['h_operatingHour'],
-                                          style: TextStyle(
-                                            fontFamily: 'Comfortaa',
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: MediaQuery.of(context).size.height * 0.02,
-                                            color: Colors.black,
+                                // Operating Hours
+                                Row(
+                                  children: [
+                                    Container(
+                                      child: Image.asset("assets/icons/clock.png", height: MediaQuery.of(context).size.height * 0.03 // Icon Size
                                           ),
+                                    ),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width * 0.8,
+                                      margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02),
+                                      child: Text(
+                                        snapshot.data.documents[index]['h_operatingHour'],
+                                        style: TextStyle(
+                                          fontFamily: 'Comfortaa',
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: MediaQuery.of(context).size.height * 0.018,
+                                          color: Colors.black,
                                         ),
                                       ),
-                                    ],
-                                  ),
+                                    ),
+                                  ],
+                                ),
 
-                                  SizedBox(height: MediaQuery.of(context).size.height * 0.025),
+                                SizedBox(height: MediaQuery.of(context).size.height * 0.01),
 
-                                  // Phone Number
-                                  Row(
-                                    children: [
-                                      Container(
-                                        child: Image.asset(
-                                          "assets/icons/hospitalNum.png",
-                                          height: MediaQuery.of(context).size.height * 0.03, // Icon Size
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02),
-                                        child: Text(
-                                          snapshot.data.documents[index]['h_tel'],
-                                          style: TextStyle(
-                                            fontFamily: 'Comfortaa',
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: MediaQuery.of(context).size.height * 0.02,
-                                            color: Colors.black,
+                                // Phone Number
+                                Row(
+                                  children: [
+                                    Container(
+                                      child: Image.asset("assets/icons/hospitalNum.png", height: MediaQuery.of(context).size.height * 0.03 // Icon Size
                                           ),
+                                    ),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width * 0.8,
+                                      margin: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02),
+                                      child: Text(
+                                        snapshot.data.documents[index]['h_tel'],
+                                        style: TextStyle(
+                                          fontFamily: 'Comfortaa',
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: MediaQuery.of(context).size.height * 0.018,
+                                          color: Colors.black,
                                         ),
                                       ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        Divider(
-                          indent: MediaQuery.of(context).size.width * 0.0,
-                          endIndent: MediaQuery.of(context).size.width * 0.0,
-                          color: Colors.black,
-                          thickness: MediaQuery.of(context).size.height * 0.002,
-                        ),
-                      ],
+                          /*
+                          Divider(
+                            indent: MediaQuery.of(context).size.width * 0.0,
+                            endIndent: MediaQuery.of(context).size.width * 0.0,
+                            color: Colors.black,
+                            thickness: MediaQuery.of(context).size.height * 0.002,
+                          ),
+                          */
+                          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                        ],
+                      ),
                     ),
                   );
                 },
