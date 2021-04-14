9d6a80af2634c7560287c93a0bc8e6e8ed466f0e
wellson99 <55526214+wellson99@users.noreply.github.com>
2021-04-15 01:23:35 +0800

    Minor updates to some module
    - Update the App Review & Bugs Report module
    - New sub main screen widget added
    - Fix on certain module didn't display the widget title

--- main/bundle_of_joy/lib/widgets/horizontalCardWidget.dart
+++ main/bundle_of_joy/lib/widgets/horizontalCardWidget.dart
@@ -72,3 +72,68 @@ class HorizontalCardWidget extends StatelessWidget {
     );
   }
 }
+
+class HorizontalCardWidgetStyle2 extends StatelessWidget {
+  final String svgSrc, title, description;
+  final Function onTap;
+
+  const HorizontalCardWidgetStyle2({
+    Key key, this.svgSrc, this.title, this.description, this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 13, horizontal: 8),
+      padding: EdgeInsets.all(10),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(13),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(15, 15), blurRadius: 20, spreadRadius: 5, color: Color(0xFFE6E6E6),
+          ),
+        ],
+      ),
+      child: InkWell(
+        onTap: onTap,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: SvgPicture.asset(svgSrc, height: 65, width: 65),
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    title,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.width * 0.045,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 4),
+                    child: Text(
+                      description,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.035,
+                        color: Colors.black.withOpacity(0.65),
+                      )
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
